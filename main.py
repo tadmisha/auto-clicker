62e03f70992400a3f7348fac12210d173e85eb87
tadmisha <tadmisha@gmail.com>
2022-08-20 16:47:49 +0400

    Setup window

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
+import tkinter as tk
+import pyautogui
 
+class Window():
+    def __init__(self):
+        self.win = tk.Tk()
+        self.win.title('auto clicker')
+        self.win.geometry('400x500')
+        self.win.resizable(width=False, height=False)
+
+
+def main():
+    win = Window()
+    win.win.mainloop()
+
+if __name__ == '__main__':
+    main()
