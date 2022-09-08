510baba59655cec06679cd646b78e338f9621e96
tadmisha <111607554+tadmisha@users.noreply.github.com>
2022-09-08 15:46:19 +0400

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import pyautogui
 class Window():
     def __init__(self):
         self.win = tk.Tk()
-        self.w, self.h = 400, 450
+        self.w, self.h = 263, 337
         self.win.title('auto clicker')
         self.win.geometry(f'{self.w}x{self.h}')
         self.win.resizable(width=False, height=False)
@@ -20,15 +20,15 @@ class Window():
         self.cooldown = tk.Entry(self.win, width=round(self.w*0.019), justify=tk.CENTER, font=('Regular', 14),
                                  validate='all', validatecommand=(self.win.register(lambda num: self.isfloat(num) or num==''), '%P'))
         self.cooldown.insert(0, '1')
-        self.second = tk.Label(self.win, text='Click interval\n(Second):', bg='#a5a5a5', font=('Regular', 7, 'bold'))
+        self.second = tk.Label(self.win, text='Click interval\n(Second):', bg='#a5a5a5', font=('Regular', 8, 'bold'))
         self.mouse_pos = tk.Label(self.win, text='X: 0\nY: 0', bg='#a5a5a5', font=('Regular', 18, 'bold'),
                                   padx=7, justify=tk.LEFT, anchor='w', width=round(self.w*0.0187))
-        self.mouse_but_text = tk.Label(self.win, text='Mouse button', bg='#a5a5a5', font=('Regular', 8, 'bold'))
+        self.mouse_but_text = tk.Label(self.win, text='Mouse button:', bg='#a5a5a5', font=('Regular', 8, 'bold'))
         self.mouse_but_val = tk.StringVar(self.win, value='left')
         self.mouse_but = tk.OptionMenu(self.win, self.mouse_but_val, *['left', 'middle', 'right'])
         self.repeat_s = tk.Radiobutton(self.win, text='Repeat until stopped', variable=self.repeat_val, value=1)
-        self.repeat_c = tk.Radiobutton(self.win, text='Repeat', variable=self.repeat_val, value=2)
-        self.repeat_ct = tk.Entry(self.win,  width=round(self.w*0.018), justify=tk.CENTER, font=('Regular',13),
+        self.repeat_c = tk.Radiobutton(self.win, width=round(self.w*0.0225), text='Repeat', variable=self.repeat_val, value=2)
+        self.repeat_ct = tk.Entry(self.win, width=round(self.w*0.0228), justify=tk.CENTER, font=('Regular',13),
                                   validate='all', validatecommand=(self.win.register(lambda num: num.isdigit() or num == ''), '%P'))
         self.repeat_ct.insert(0, '10')
         self.start = tk.Button(self.win, text='Start (Shift+S)', bg='white',
@@ -36,16 +36,16 @@ class Window():
         self.stop = tk.Button(self.win, state=tk.DISABLED, text='Stop (Shift+S)', bg='white',
                               font=('Regular', 25, 'bold'), command=self.btn)
 
-        self.cooldown.place(x=self.w*0.655, y=self.h*0.015)
-        self.second.place(x=self.w*0.395, y=self.h*0.0125)
-        self.mouse_but_text.place(x=self.w*0.021, y=self.h*0.01)
-        self.mouse_but.place(x=self.w*0.021, y=self.h*0.0775)
-        self.repeat_s.place(x=self.w*0.021, y=self.h*0.225)
-        self.repeat_c.place(x=self.w*0.021, y=self.h*0.35)
-        self.repeat_ct.place(x=self.w*0.25, y=self.h*0.35)
-        self.mouse_pos.place(x=self.w*0.5575, y=self.h*0.225)
-        self.start.place(x=self.w*0.021, y=self.h*0.5)
-        self.stop.place(x=self.w*0.021, y=self.h*0.75)
+        self.cooldown.place(x=self.w*0.75, y=self.h*0.0215)
+        self.second.place(x=self.w*0.4375, y=self.h*0.022)
+        self.mouse_but_text.place(x=self.w*0.021, y=self.h*0.02)
+        self.mouse_but.place(x=self.w*0.021, y=self.h*0.12)
+        self.repeat_s.place(x=self.w*0.021, y=self.h*0.255)
+        self.repeat_c.place(x=self.w*0.021, y=self.h*0.38)
+        self.repeat_ct.place(x=self.w*0.325, y=self.h*0.38)
+        self.mouse_pos.place(x=self.w*0.62, y=self.h*0.255)
+        self.start.place(x=self.w*0.021, y=self.h*0.56)
+        self.stop.place(x=self.w*0.021, y=self.h*0.78)
         self.win.bind('<Key>', lambda val: self.btn() if val.keysym=='S' else None)
 
         self.change_coord()
@@ -64,6 +64,7 @@ class Window():
             if val[self.mouse_but_val.get()] == 1: pyautogui.leftClick()
             if val[self.mouse_but_val.get()] == 2: pyautogui.middleClick()
             if val[self.mouse_but_val.get()] == 3: pyautogui.rightClick()
+
             self.win.after(round(float(self.cooldown.get())*1000), self.click)
             self.count+=1
         else:
